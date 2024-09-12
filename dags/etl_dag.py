8fa2434e5f206305d7e2647b81ccdb77c077b397
NicolasAlarconS <nicolas.alarcon.k@gmail.com>
2024-09-11 21:01:01 -0300

    Update

--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -17,7 +17,7 @@ from modules import extract, transform, load, email_notification
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 8, 30),
+    'start_date': datetime.now(),
     'retries': 1,
     'retry_delay': timedelta(minutes=1),
     'on_failure_callback': email_notification.handle_dag_status,
